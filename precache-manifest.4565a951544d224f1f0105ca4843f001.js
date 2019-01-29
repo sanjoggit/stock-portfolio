c4a93a0238c6176ea4ddf9022745f0742c9423d1
sanjoggit <sanjogstha7@gmail.com>
2019-01-29 12:58:37 +0200

    Updates

---
+++ precache-manifest.4565a951544d224f1f0105ca4843f001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d45b19730ea7d8596e0",
+    "url": "/stock-portfolio/static/js/runtime~main.6d45b197.js"
+  },
+  {
+    "revision": "42e54d4b35ebffc7d529",
+    "url": "/stock-portfolio/static/js/main.42e54d4b.chunk.js"
+  },
+  {
+    "revision": "bec4ce42845a0b267023",
+    "url": "/stock-portfolio/static/js/1.bec4ce42.chunk.js"
+  },
+  {
+    "revision": "42e54d4b35ebffc7d529",
+    "url": "/stock-portfolio/static/css/main.1af57a88.chunk.css"
+  },
+  {
+    "revision": "bec4ce42845a0b267023",
+    "url": "/stock-portfolio/static/css/1.8ae84eae.chunk.css"
+  },
+  {
+    "revision": "f274b83628c4e306960c142246fa3d52",
+    "url": "/stock-portfolio/index.html"
+  }
+];
